a4750816d2a77f6f695b3c24c87b12592070df52
Eric Bernstine <ericbernstine@gmail.com>
2024-11-29 11:30:23 -0500

    fixes to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Books from './pages/Books';
 import Author from './pages/Author';
-import './App.css';
 
-export default function App(){
-  return(
-    <BrowserRouter basename="/stormlightReactApp">
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path='Books' element={<Books />}/>
-          <Route path='Author' element={<Author />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+export default function App() {
+    const basename = process.env.NODE_ENV === 'production' ? '/stormlightReactApp' : '/';
+
+    return (
+        <BrowserRouter basename={basename}>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="books" element={<Books />} />
+                    <Route path="author" element={<Author />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
